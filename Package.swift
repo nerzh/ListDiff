5a11eb4a37b3969cd8ebd95e02c8683ad1c3d801
OleG <emptystamp@gmail.com>
2020-04-11 09:40:57 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "ListDiff"
+    name: "ListDiff",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "ListDiff",
+            targets: ["ListDiff"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "ListDiff",
+            dependencies: [
+            ]
+        )
+    ]
 )
